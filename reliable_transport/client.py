070a12fd9b9d6b2c53b1d662eb7425ed5ee91248
Nate Collings <nate@natecollings.com>
2015-03-19 17:07:20 -0600

    setup client handshake

--- reliable_transport/client.py
+++ reliable_transport/client.py
@@ -3,6 +3,7 @@ from udp import udp
 from header import Header
 
 GOBACKN = True
+STARTING_SEQN = 0
 
 class Client:
     def __init__(self, host, port, packet_size=500):
@@ -12,6 +13,7 @@ class Client:
         self.udp_connection = udp.Udp(self.port+1)
 
     def transmit_file(self, filename):
+        self._handshake()
         if GOBACKN is True:
             self.go_back_n(filename)
         else:
@@ -21,7 +23,7 @@ class Client:
         with open(filename, 'r') as f:
             while True:
                 data = f.read(500-Header.size())
-                header = Header(1, 1, 1, 1, data)
+                header = Header(1, 1, 1, data)
                 if len(header.formatted) != Header.size():
                     raise Exception('Header size is wrong: should be {}, was {}'.format(Header.size(), len(header.formatted)))
                 if not data:
@@ -30,3 +32,25 @@ class Client:
 
     def selective_repeat(self, filename):
         pass
+
+    def _handshake(self):
+        self._send_syn()
+        received_seqn = self._wait_for_syn_ack()
+        self._send_ack(received_seqn)
+
+    def _send_syn(self):
+        header = Header(STARTING_SEQN, 0, 5, '', syn=True)
+        self.udp_connection.send_packet(header.formatted, self.host, self.port)
+
+    def _wait_for_syn_ack(self):
+        syn_ack = self.udp_connection.recv()
+        header = Header.parse(syn_ack[:Header.size()])
+        if header.ackn != STARTING_SEQN+1:
+            raise Exception('Incorrect received seqn: should be {}, was {}'.format(STARTING_SEQN+1, header.ackn))
+        if header.syn is False or header.ack is False:
+            raise Exception('The server did not respond with a SYN-ACK')
+        return header.seqn
+
+    def _send_ack(self, received_seqn):
+        header = Header(STARTING_SEQN+1, received_seqn+1, 5, '', ack=True)
+        self.udp_connection.send_packet(header.formatted, self.host, self.port)
--- reliable_transport/header.py
+++ reliable_transport/header.py
@@ -2,25 +2,25 @@ import struct
 import hashlib
 
 # https://docs.python.org/2/library/struct.html#format-characters
-FORMAT_STR = '!LLhh32s'
+FORMAT_STR = '!LLh32s????'
 
 class Header:
-    def __init__(self, seqn, ackn, window_size, flags, data):
-        self.seqn = seqn                        #  4 bytes
-        self.ackn = ackn                        #  4 bytes
-        self.window_size = window_size          #  2 bytes
-        self.flags = flags                      #  2 bytes
-        self.checksum = Header.checksum(data)   # 32 bytes
-        self.formatted = struct.pack(FORMAT_STR, self.seqn, self.ackn, self.window_size, self.flags, self.checksum)
+    def __init__(self, seqn, ackn, window_size, data, syn=False, ack=False, fin=False, file_name=False):
+        self.seqn = seqn                                             #  4 bytes
+        self.ackn = ackn                                             #  4 bytes
+        self.window_size = window_size                               #  2 bytes
+        self.checksum = Header.checksum(data)                        # 32 bytes
+        self.flags = [syn, ack, fin, file_name]                      #  4 bytes
+        self.formatted = struct.pack(FORMAT_STR, self.seqn, self.ackn, self.window_size, self.checksum, *self.flags)
 
     @staticmethod
     def parse(header):
         unpacked = struct.unpack(FORMAT_STR, header)
-        return Header(unpacked[0], unpacked[1], unpacked[2], unpacked[3], unpacked[4])
+        return Header(*unpacked)
 
     @staticmethod
     def size():
-        return 44
+        return 46
 
     @staticmethod
     def checksum(data):
