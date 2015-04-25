9b99b50cf083e22f423a5ecdd508d7cdbdfc4e0d
Nate Collings <nate@natecollings.com>
2015-04-25 02:54:03 -0600

    more server updates

--- rpc/server.py
+++ rpc/server.py
@@ -1,6 +1,7 @@
 from interface import Interface
 from marshaller import Marshal
 from reliable_transport import server
+from reliable_transport import client
 
 class MethodImplementations(Interface):
     def add(a, b):
@@ -10,16 +11,18 @@ class MethodImplementations(Interface):
         return a - b
 
 class Server:
-    def __init__(self, port):
+    def __init__(self, address, port):
         self.methods = MethodImplementations()
         self._register_methods()
         self.marshal = Marshal()
         self.server = server.Server(port)
+        self.client = client.Client(address, port+10)
 
     def receive(self):
-        self.server.receive_loop()
-        data = ('add', 1, 1)
-        return self._call(data[0], *data[1:])
+        data = self.server.receive_loop()
+        data = self._unmarshal(data)
+        # data = ('add', 1, 1)
+        return self._call(data[0], *data[1:]) # data[0] is the func, the rest of the arr is the args
 
     def _unmarshal(self, data):
         return self.marshal.unmarshal(data)
@@ -29,7 +32,7 @@ class Server:
 
     def _send(result):
         marshalled = self.marshal.marshal(result)
-        print 'sending result'
+        self.client.transmit_data(marshalled)
 
     def _register_methods(self):
         methods = [x for x in dir(MethodImplementations) if x[0:1] != '_']
@@ -37,5 +40,5 @@ class Server:
         for method in methods:
             self.method_impls[method] = getattr(self.methods, method)
 
-server = Server(1234)
+server = Server(1234, 'localhost')
 server.receive()
